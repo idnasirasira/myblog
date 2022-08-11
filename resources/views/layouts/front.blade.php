209cfb91262fbc95569f472311c0c8ccd6f3dad7
Aris Arisandi <idnasirasira@gmail.com>
2022-08-11 18:08:05 +0700

    Hide on mobile

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -21,7 +21,8 @@
         </div>
 
         @if (Route::has('login'))
-                <div class="fixed top-0 right-0 px-6 py-4 sm:block">
+
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a class="text-sky-600 text-lg hover:text-gray-600" href="{{route('post.create')}}">Create a new post</a>
                     @else
